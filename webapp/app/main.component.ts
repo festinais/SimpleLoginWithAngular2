c0d297bf2ab5f3e1e753b1cf84a3a3b0a5cea2f3
FestinaI <festina.ismajli123@gmail.com>
2016-11-28 16:20:00 +0100

    initial client side implemenation

---
+++ webapp/app/main.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  templateUrl: "../templates/login-form.html"
+})
+
+export class AppComponent {
+
+}
